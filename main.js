562b9bb5692a9d077d12e28f45c4c9037ed53c49
Diego Giuliani <giuliani.diego@gmail.com>
2022-09-18 00:12:53 -0300

    Add animations

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 import "./style.css";
+import { Mandrake } from "./mandrake";
+
+let cxt;
+
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("animation-canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 500;
+
+  const mandrake = new Mandrake(canvas.width, canvas.height);
+  document
+    .getElementById("all")
+    .addEventListener("click", () => mandrake.setAnimation(0, 355));
+  document
+    .getElementById("grow")
+    .addEventListener("click", () => mandrake.setAnimation(0, 75));
+  document
+    .getElementById("wink")
+    .addEventListener("click", () => mandrake.setAnimation(76, 112));
+  document
+    .getElementById("float")
+    .addEventListener("click", () => mandrake.setAnimation(113, 262));
+  document
+    .getElementById("hide")
+    .addEventListener("click", () => mandrake.setAnimation(263, 355));
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    mandrake.update();
+    mandrake.draw(ctx);
+
+    window.requestAnimationFrame(animate);
+  }
+  animate();
+});
---
+++ mandrake.js
@@ -0,0 +1,42 @@
+export class Mandrake {
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.image = document.getElementById("mandrake");
+    this.spriteWidth = 256;
+    this.spriteHeight = 256;
+    this.width = this.spriteWidth;
+    this.height = this.spriteHeight;
+    this.x = this.canvasWidth / 2 - this.width / 2;
+    this.y = this.canvasHeight - this.height;
+    this.minFrame = 0;
+    this.maxFrame = 355;
+    this.frame = 0;
+    this.frameX = 3;
+    this.frameY = 10;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.frameX * this.spriteWidth,
+      this.frameY * this.spriteHeight,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+  setAnimation(minFrame, maxFrame) {
+    this.minFrame = minFrame;
+    this.maxFrame = maxFrame;
+    this.frame = minFrame;
+  }
+  update() {
+    this.frame = this.frame < this.maxFrame ? this.frame + 1 : this.minFrame;
+    this.frameX = this.frame % 18;
+    this.frameY = Math.floor(this.frame / 18);
+  }
+}
